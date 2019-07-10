1db144e85d6748b7fca25886819fb82c3d57b177
Jose Silva <zeeeee.miguel@gmail.com>
2019-07-11 00:52:30 +0100

    percentage bar

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ var endTime = end.getTime()/1000;
 function updateBar() {
 	currTime = Date.now()/1000;
 	percent = (currTime - startTime) / (endTime - startTime) * 100;
-    document.getElementById("percentage").innerHTML = percent;
+    document.getElementById("percentage").innerHTML = percent.toFixed(4);
 }
 
 var timer = setInterval(updateBar, 1000);
